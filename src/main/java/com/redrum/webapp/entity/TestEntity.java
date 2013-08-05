7c1c4276b046a64ab80abb9a85279aa1da05bad1
jobpassion <jobpassion@gmail.com>
2013-08-05 16:08:48 +0800

    Signed-off-by: jobpassion <jobpassion@gmail.com>

---
+++ src/main/java/com/redrum/webapp/entity/TestEntity.java
@@ -0,0 +1,14 @@
+package com.redrum.webapp.entity;
+
+public class TestEntity {
+	private String str;
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+
+}
--- src/main/java/com/redrum/webapp/webController/MainController.java
+++ src/main/java/com/redrum/webapp/webController/MainController.java
@@ -7,7 +7,11 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.redrum.webapp.entity.TestEntity;
 @Controller
+@RequestMapping("/abc")
 public class MainController {
 	   @RequestMapping(value="/test")
 	    public String test() {
@@ -15,6 +19,18 @@ public class MainController {
 		   Map m = new HashMap();
 		   m.put("abf", 123);
 		   return "test";
+	    }
+	   
+	   
+	   @RequestMapping(value="/test2", produces="application/json")
+	    public  TestEntity test2() {
+		   System.out.println("hello world");
+		   Map m = new HashMap();
+		   m.put("abf", 123);
+		   
+		   TestEntity te = new TestEntity();
+		   te.setStr("abc");
+		   return te;
 	    }
 
 }
