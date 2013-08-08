585caf6a0e8c95f33a2399443c7c5356ae637b75
jobpassion <jobpassion@gmail.com>
2013-08-08 13:10:01 +0800

    Signed-off-by: jobpassion <jobpassion@gmail.com>

--- src/main/java/com/redrum/webapp/service/TestService.java
+++ src/main/java/com/redrum/webapp/service/TestService.java
@@ -1,47 +1,72 @@
 package com.redrum.webapp.service;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Service;
 
+import com.ibatis.sqlmap.client.SqlMapClient;
 import com.redrum.webapp.dao.DaoHelper;
-public class TestService {
+@Service
+public class TestService  implements  InitializingBean{
+	@Autowired
 	private DaoHelper daoHelper;
+	@Autowired
+	private SqlMapClient sqlMapClient;
 	
-	public void setDaoHelper(DaoHelper daoHelper) {
-		this.daoHelper = daoHelper;
+
+	public void init(){
+
+//		try {
+//			daoHelper.getSqlMapClient().queryForList("T.test2");
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+
+
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		init();
+	}
+
+
+	public List queryAllTest() throws SQLException {
+		// TODO Auto-generated method stub
+		return sqlMapClient.queryForList("T.queryAllTest");
 	}
 	
-	class TestJob implements Runnable{
+	public static class T{
+		private String name;
 
-		public void run() {
-			// TODO Auto-generated method stub
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+		public String getName() {
+			return name;
+		}
 
-//			System.out.println(daoHelper.getSqlMapClientTemplate().
-//					queryForList("test1", new Object()));
-			try {
-				daoHelper.getSqlMapClient().queryForList("T.findAllStudent");
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		public void setName(String name) {
+			this.name = name;
 		}
 		
 	}
-
-	public void init(){
-		
-		TestJob t = new TestJob();
-		Thread th = new Thread(t);
-		th.start();
+	
+	public static void main(String[] args) {
+		ExpressionParser parser = new SpelExpressionParser();
+		Expression exp = parser.parseExpression("name == 'abc'");
+		T t = new T();
+		t.name = "abc";
+		EvaluationContext context = new StandardEvaluationContext(t);
+		boolean result = exp.getValue(context, Boolean.class);  // evaluates to true
+		System.out.println(result);
 	}
 
 }
--- src/main/java/com/redrum/webapp/webController/MainController.java
+++ src/main/java/com/redrum/webapp/webController/MainController.java
@@ -1,46 +1,58 @@
 package com.redrum.webapp.webController;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.redrum.webapp.entity.TestEntity;
+import com.redrum.webapp.service.TestService;
+
 @Controller
 public class MainController {
-	   @RequestMapping(value="/test")
-	    public String test() {
-		   System.out.println("hello world");
-		   Map m = new HashMap();
-		   m.put("abf", 123);
-		   return "test";
-	    }
-	   
-	   
-	   @RequestMapping(value="/test2", produces="application/json")
-	   @ResponseBody
-	    public  TestEntity test2() {
-		   System.out.println("hello world");
-		   Map m = new HashMap();
-		   m.put("abf", 123);
-		   
-		   TestEntity te = new TestEntity();
-		   te.setStr("abcd");
-		   return te;
-	    }
-	   
+	@Autowired
+	private TestService testService;
 
-	   @RequestMapping(value="/test3")
-	   @ResponseBody
-	    public String test3() {
-		   System.out.println("hello world");
-		   Map m = new HashMap();
-		   m.put("abf", 123);
-		   return "ÃÏ·²»ªÑ¾Ñ¾";
-	    }
+	@RequestMapping(value = "/test")
+	public String test() {
+		System.out.println("hello world");
+		Map m = new HashMap();
+		m.put("abf", 123);
+		return "test";
+	}
+
+	@RequestMapping(value = "/test2", produces = "application/json")
+	@ResponseBody
+	public TestEntity test2() {
+		System.out.println("hello world");
+		Map m = new HashMap();
+		m.put("abf", 123);
+
+		TestEntity te = new TestEntity();
+		te.setStr("abcd");
+		return te;
+	}
+
+	@RequestMapping(value = "/test3")
+	@ResponseBody
+	public String test3() {
+		System.out.println("hello world");
+		Map m = new HashMap();
+		m.put("abf", 123);
+		return "mengfanhua";
+	}
+
+	@RequestMapping(value = "/test4")
+	@ResponseBody
+	public List test4() throws SQLException {
+		List l = testService.queryAllTest();
+		return l;
+	}
 
 }
